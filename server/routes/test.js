8cef03ce5f5951da8afab0caec376192e6150b8d
s <saikat626@gmail.com>
2018-09-16 00:36:56 +0530

    Added firebase. Index router to mount others routers and initialize firebase

---
+++ server/routes/test.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const { execSync } = require('child_process');
+
+router.get('/user', function(req, res){
+  if (req.user) {
+	  res.send("Logged in as: "+req.user.username);
+  }else {
+	  res.send("Not logged in");
+  }
+});
+
+router.get('/CI', function(req, res) {
+	const commitLog = execSync('git log -n 1').toString('utf8');
+	res.send(commitLog);
+});
+
+router.get('/firebase', function(req, res) {
+	const admin = require('firebase-admin');
+
+	var db = admin.database();
+	var ref = db.ref('notifications/mobile');
+
+	ref.on('value', function(snapshot) {
+			res.send(snapshot.val());
+		}, function(errorObj) {
+			res.send(errorObj.code);
+	});
+
+});
+
+module.exports = router;
