b93244f87dfcb8507b9107d4516a2b6929070dd5
Saikat Dey <saii@localhost.localdomain>
2018-10-10 00:57:55 +0530

    Microservicing this big server

--- README.md
+++ README.md
@@ -14,10 +14,36 @@ Node and  empathy installed globally on your machine.
 2. Install [node](https://nodejs.org/en/download/) `nvm install 8.12.0`
 3. Install [empathy](https://github.com/PolymerLabs/empathy/tree/initial-implementation) globally `npm install @0xcda7a/empathy -g`
 
+
 ### Install the dependencies
 Change directory into the new project you just cloned, then install dependencies.
 ```bash
 $ npm install
 $ cd public
 $ npm install
+```
+
+### Run the application
+```bash
+$ node server/app
+Server started on port: 8000
+```
+
+### Register and mount a microservice
+Perform a post request. This will redirect all requests `http://localhost:8080/mount_path/*` to `http://server_ip/*`.
+```javascript
+const form = {
+    name: 'name_of_the_service',
+    path: 'mount_path',
+    ip: 'server_ip'
+};
+request.post('http://localhost:8000/register', {form: form});
+```
+
+
+### Enable/Disable a microservice
+Perform a get request. User must be logged in to perform these.
+```javascript
+request.get('http://localhost:8000/enable/service') // Will enable sevice
+request.get('http://localhost:8000/disable/service') // Will disable sevice
 ```
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,96 @@
 const express = require('express');
 const path = require('path');
+const request = require('sync-request');
 const router = express.Router();
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer();
 
-const test = require('./test');
-router.use('/test', test);
 
-const files = require('./files');
-router.use('/files', files);
+var microserviceDB = [];
+
+router.post('/register', function(req, res) {
+  const mountPath = req.body.path;
+  const proxyIP = req.body.ip;
+  const name = req.body.name;
+
+  for (var service of microserviceDB) {
+    if (service.name === name) {
+      service.path = mountPath;
+      service.redirect = proxyIP;
+      res.status(204);
+      res.end();
+      return;
+    }
+  }
+  microserviceDB.push({
+              path: mountPath,
+              redirect: proxyIP,
+              name: name,
+              enabled: true
+            });
+  res.status(200);
+  res.end();
+  return;
+});
+
+router.get('/enable/:name', function(req, res) {
+  if (!req.user) {
+    res.status(401);
+    res.send("Login to enable/disable services");
+    return;
+  }
+  for (var service of microserviceDB) {
+    if (service.name === req.params.name) {
+      service.enabled = true;
+      res.send("Enabled "+service.name);
+      return;
+    }
+  }
+  res.send("Service not found");
+});
+
+router.get('/disable/:name', function(req, res) {
+  if(!req.user) {
+    res.status(401);
+    res.send("Login to enable/disable services");
+    return;
+  }
+  for (var service of microserviceDB) {
+    if (service.name === req.params.name) {
+      service.enabled = false;
+      res.send("Disabled "+service.name);
+      return;
+    }
+  }
+  res.send("Service not found");
+});
+
+router.use('/', function(req, res) {
+  const pathRoute = req.url.split('/');
+  if (pathRoute.length == 0) {
+    res.send("No microservice on root");
+    return;
+  }
+
+  var isFound = false;
+  for(var service of microserviceDB) {
+    if (service.path === pathRoute[1] && service.enabled) {
+      console.log("Using service "+service.name);
+      isFound = true;
+      req.url = req.url.substring(pathRoute[1].length+1);
+      var headerUser = req.user;
+      if (headerUser) {
+        delete headerUser.password;
+      }
+      proxy.web(req, res, {target: service.redirect, headers: {user: JSON.stringify(headerUser) || "Anonymous"}}, function(err) {
+        console.log(service.name+" error: "+err);
+      });
+      break;
+    }
+  }
+  if (!isFound) {
+    res.send("No matching microservice found");
+  }
+})
 
 module.exports = router;
